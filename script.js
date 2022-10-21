3bee6924b17ce089ff987a8b0fe95afda12e74b7
Madhuja93 <madhujapabasara@gmail.com>
2022-10-21 15:24:09 +0530

    Adding smooth scroll JS

--- script.js
+++ script.js
@@ -6,4 +6,7 @@ function showMenu() {
     var navLinks = document.getElementById("navLinks");
     navLinks.style.right = "-200px";
   }
-  
+  var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000,
+    speedAsDuration: true
+  });
