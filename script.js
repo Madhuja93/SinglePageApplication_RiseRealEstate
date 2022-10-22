e4930e28c40215a87589e892c05011c45158f225
Madhuja93 <madhujapabasara@gmail.com>
2022-10-22 10:43:36 +0530

    Adding Parallax to the About page

--- script.js
+++ script.js
@@ -6,7 +6,21 @@ function showMenu() {
     var navLinks = document.getElementById("navLinks");
     navLinks.style.right = "-200px";
   }
-  var scroll = new SmoothScroll('a[href*="#"]', {
+
+  /*var scroll = new SmoothScroll('a[href*="#"]', {
     speed: 1000,
     speedAsDuration: true
-  });
+  }); */
+
+const li=document.querySelectorAll("links");
+const sec=document.querySelectorAll("section");
+
+function activeMenu(){
+  let len=sec.length;
+  while(--len && window.scrollY+ 97 < sec[len].offsetTop){
+    li.forEach(Itx => Itx.classList.remove("active"));
+    li[len].classList.add("active");
+  }
+  activeMenu();
+  window.addEventListener("scrol", activeMenu);
+}
